48efbeff8439d5aa9b8cf79478642b7ada896cfd
Tyson Unce <Tyson@MacBook-Pro.attlocal.net>
2022-05-11 21:33:40 -0500

    pushing

--- WeeklyCodingChallenge_6/Very_Hard/script.js
+++ WeeklyCodingChallenge_6/Very_Hard/script.js
@@ -1,34 +1,19 @@
-let stringy= "(hello world(";
-
-
-
-let checkBrack = p1 => {
-    let brackArray = ["{", "}", "(", ")", "[", "]"]
-    if (brackArray[0] == p1[0]){
-        if (brackArray[1] == p1[p1.length-1]) {
-            return console.log(true)}
-            else if (brackArray[1] != p1[p1.length-1]){
-                return console.log(false)
-            }
-
-}
-else if (brackArray[2] == p1[0]){
-    if (brackArray[3] == p1[p1.length-1]) {
-        return console.log(true)}
-        else if (brackArray[3] != p1[p1.length-1]){
-            return console.log(false)
+const coinFunc = function(p1,p2){
+    let list1 = []
+    let max = Math.max(...p1)
+    for (let i of p1){
+        if (Math.floor(p2/max) > 0 ){
+           list1.push(i)
         }
+        
+    }
+    
 
 }
-else if (brackArray[4] == p1[0]){
-    if (brackArray[5] == p1[p1.length-1]) {
-        return console.log(true)}
-        else if (brackArray[5] != p1[p1.length-1]){
-            return console.log(false)
-        }
 
-}
-return console.log(brackArray[0])
-}
+let coins = [2,3,5,6,7,6,5,3,]
 
-checkBrack(stringy)
+
+coinFunc(coins,60)
+
+console.log(Math.floor(60/))
